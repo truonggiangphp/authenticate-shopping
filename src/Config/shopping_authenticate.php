c3f1b806d5456148f67ff7f6d42cf5e2107e5fb2
GiangBeo <giangbeoit@gmail.com>
2020-07-15 13:47:24 +0700

    Update

--- src/Config/shopping_authenticate.php
+++ src/Config/shopping_authenticate.php
@@ -4,8 +4,9 @@ use Webikevn\AuthenticateShopping\Models\TblSession;
 
 return [
     'monoris_cookie_name' => 'monoris9',
+    'monoris_header_name' => 'auth-monoris',
     'domain' => env('WEBIKE_DOMAIN', 'www.webike.net'),
     'enable' => true,
-    'auth_driver' => 'api',
+    'auth_driver' => 'web',
     'monoris_authenticate_type' => TblSession::MONORIS_COOKIE
 ];
